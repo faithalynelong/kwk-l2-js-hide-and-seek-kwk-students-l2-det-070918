82e048a4dd1fbaf2362289aab3229ec94f1f3688
faithalynelong <falong313@pccsk12.com>
2018-07-12 20:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //Code your solution here
+function getFirstSelector(s){
+  return document.querySelector
+}
+
+function nestedTarget(){
+  
+}
+
+increaseRankBy(n);{
+  var ijj = parseInt()
+}
+deepestChild(){
+  
+}
